db49d9cefb9d8bb5b5b09e96f9ed7f38028a3613
Simon <bardrabbit@gmail.com>
2020-07-30 02:48:58 -0400

    Updates

---
+++ precache-manifest.214b7b36d338dd2cc080f534ca987081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03148f00b994fd5b767f5d77dc3a5ce5",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "69a8a132a1bf31a051d8",
+    "url": "/client/static/js/2.7b3bd8be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/client/static/js/2.7b3bd8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508ba5ce5e918ce4f251",
+    "url": "/client/static/js/main.1475b0d4.chunk.js"
+  },
+  {
+    "revision": "d64b09180f5230249a46",
+    "url": "/client/static/js/runtime-main.1d4c076d.js"
+  }
+]);
