6889e1bd27f96ef9a970170c963b72971f0b09bf
Simon <bardrabbit@gmail.com>
2020-07-30 22:36:41 -0400

    record user id

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,7 +11,7 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                
+
                 this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
@@ -21,7 +21,7 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = (isSignedIn) => {
         if (isSignedIn) {
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         } else {
             this.props.signOut();
         }
