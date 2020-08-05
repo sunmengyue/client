e6e7e390c2de717917f88f4e3dd96028ec2dc349
Simon <bardrabbit@gmail.com>
2020-08-05 00:52:31 -0400

    complete stream action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
+import { SIGN_IN, 
+         SIGN_OUT, 
+         CREATE_STREAM,
+         FETCH_STREAMS,
+         FETCH_STREAM, 
+         DELETE_STREAM,
+         EDIT_STREAM } from './types';
+
 
 export const signIn = (userId) => {
     return {
@@ -12,3 +20,30 @@ export const signOut = () => {
         type: SIGN_OUT
     };
 };
+
+export const createStream = (formValues) => {
+    return async (dispatch) => {
+        const response = await streams.post('/streams', formValues);
+        dispatch({ type: CREATE_STREAM, payload: response.data });
+    };
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({type:FETCH_STREAMS, payload: response.data});
+};
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data});
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data});
+};
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id});
+}
--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component{
     renderError({ touched, error}) {
@@ -19,14 +21,14 @@ class StreamCreate extends React.Component{
             <div className={className}>
                 <label>{label}</label>
                 <input {...input} autoComplete="off"/>
-                <div>{this.renderError(meta)}</div>
+                {this.renderError(meta)}
             </div>
             
         )
     }
 
-    onSubmit() {
-        
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -53,7 +55,9 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
