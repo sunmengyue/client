f57dd076a9d42798b31e8fab4f73b1513fda27fc
Simon <bardrabbit@gmail.com>
2020-08-05 21:03:19 -0400

    link to stream creation

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
-
-
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
     renderAdminButtons(stream) {
@@ -37,13 +36,24 @@ class StreamList extends React.Component {
         });
     }
 
-    
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return(
+                <div style= {{textAlign: "right"}}>
+                    <Link to="/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
 
     render() {
         return (
             <div>
                 <h2>Streams</h2>
-                <div className="ui celled list">{this.renderList()}</div>
+                <div className="ui celled list">{this.renderList()}</div>     
+                {this.renderCreate()}
             </div>)
     }
 }
@@ -51,7 +61,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
     return { 
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn:state.auth.isSignedIn
     };
 }
 
